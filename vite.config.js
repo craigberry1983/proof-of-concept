a651c94a884f484448daaeb483f0e7511206b365
craigberry1983 <craigberry1983@gmail.com>
2025-07-30 02:59:38 -0700

    Remove dev server statements

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/proof-of-concept/',
-  server: {
-    host: true,
-    cors: true,
-  },
   plugins: [react()],
 })
 
